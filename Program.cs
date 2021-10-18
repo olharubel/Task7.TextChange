5048a1da0907cddfab6aaa8b4a64fdce13ef28e1
Ольга Рубель <rubel.olha@gmail.com>
2021-10-18 10:22:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7.TextChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = "I go,to school!     *      Girl runs to school?";
+                Console.WriteLine("Text before translating:");
+                Console.WriteLine(text);
+                Translator translator = new Translator();
+                translator.InitializeDictionary("I", "boy");
+                translator.InitializeDictionary("go", "run");
+                translator.InitializeDictionary("to", "to");
+                translator.InitializeDictionary("school", "cinema");
+                Console.WriteLine("\nText after translating:\n");
+                string translatedText = translator.TranslateText(text);
+                Console.WriteLine(translatedText);
+                
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7.TextChange
+{
+    class Translator
+    {
+        private Dictionary<string, string> dictionary;
+
+        public Translator()
+        {
+            dictionary = new Dictionary<string, string>();
+        }
+
+        public Translator(Dictionary<string, string> dictionary)
+        {
+            this.dictionary = dictionary;
+        }
+
+        public void InitializeDictionary(string initWord, string replaceWord)
+        {
+            if (!dictionary.ContainsKey(initWord))
+            {
+                dictionary.Add(initWord, replaceWord);
+            }
+        }
+
+        private StringBuilder ToStringBuilder(string text)
+        {
+            return new StringBuilder(text);
+        }
+
+        private string StringBuilderToString(StringBuilder text)
+        {
+            return text.ToString();
+        }
+
+        private char[] GetSeparators(string str)
+        {
+            List<char> separators = new List<char>();
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (!char.IsLetterOrDigit(str[i]))
+                {
+                    separators.Add(str[i]);
+                }
+            }
+
+            char[] uniqueSeparators = separators.Distinct().ToArray<char>();
+            return uniqueSeparators;
+        }
+
+        private string UserRequest(string initWord)
+        {
+            Console.WriteLine($"Enter word to replace \"{initWord}\"");
+            string replaceWord = Console.ReadLine();
+            return replaceWord;
+        }
+
+        public string TranslateText(string text)
+        {
+            if(text.Length == 0)
+            {
+                throw new FormatException("Text is empty. Impossible to translate!");
+            }
+            StringBuilder sb = new StringBuilder();
+            sb = ToStringBuilder(text);
+            string result = "";
+
+            char[] separators = GetSeparators(text);
+
+            string[] allWords = text.Split(separators);
+            string[] uniqueWords = allWords.Where(word => !string.IsNullOrEmpty(word)).ToArray<string>();
+
+            for (int j = 0; j < uniqueWords.Length; ++j)
+            {
+                if (dictionary.ContainsKey(uniqueWords[j]))
+                {
+                    sb.Replace(uniqueWords[j], dictionary[uniqueWords[j]]);
+                }
+                else
+                {
+                    string replaceWord = UserRequest(uniqueWords[j]);
+                    sb.Replace(uniqueWords[j], replaceWord);
+                }
+
+            }
+            result = StringBuilderToString(sb);
+
+            return result;
+        }
+    }
+}
